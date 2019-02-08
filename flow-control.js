3950bae08d7af7488bf0b55aacd007d9ea7c03b1
Blair Murphy <bomurf@hotmail.com>
2019-02-08 14:30:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,35 +1,35 @@
 function basicTeenager (age) {
   if (age === 13-19) {
-    return ("You are a teenager!")
+    return "You are a teenager!"
   }
 }
 
 function teenager (age) {
   if (age === 13-19) {
-    return ("You are a teenager!")
+    return "You are a teenager!"
   } else {
-    return ("You are not a teenager")
+      return "You are not a teenager"
   }
 }
 
 function ageChecker (age) {
   if (age === 13-19) {
-    return ("You are a teenager!")
+    return "You are a teenager!"
   } else if (age <= 12) {
-    return ("You are a kid")
+      return "You are a kid"
   } else if (age >= 20) {
-    return ("You are a grownup")
+      return "You are a grownup"
   } else {
-    return ("You are not a teenager")
+      return "You are not a teenager"
   }
 }
 
 function ternaryTeenager (age) {
   if (age === "13-19") {
-    return ("You are a teenager")
+    return "You are a teenager"
   }
 
-  return ("You are not a teenager")
+  return "You are not a teenager"
 }
 
 function switchAge (age) {
