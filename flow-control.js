cb142ec12351a80aa81286c1b0eb27d69d5dff16
Blair Murphy <bomurf@hotmail.com>
2019-02-08 13:49:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,3 +23,11 @@ function ageChecker (age) {
     return "You are not a teenager"
   }
 }
+
+function ternaryTeenager (age) {
+  if (age === 13-19) {
+    return "You are a teenager"
+  } 
+
+  return "You are not a teenager"
+}
